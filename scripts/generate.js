a84dd35f53436be299902df0b567a64ba8395bc8
Celia Marshall <celia.m@gmail.com>
2018-12-06 09:54:45 -0800

    post an invoice

--- scripts/generate.js
+++ scripts/generate.js
@@ -5,43 +5,37 @@ function init() {
   form.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    const userEmail = {
-      'email': event.target.email.value
-    }
-
-    let clientId 
+    const userEmail = event.target.email.value
     
-    console.log(userEmail)
     getUserByEmail(userEmail)
     .then( response => {
-      clientId = response.data.data.id
+      const clientId = response.data.data.id
+      
+      const newInvoice = {
+        'invoice_number': event.target.invoiceNumber.value,
+        'due_date': event.target.dueDate.value,
+        'notes': event.target.notes.value,
+        'clientId': parseInt(clientId)
+      }
+      
+      return createInvoice(newInvoice)
     })
-    
-    const newInvoice = {
-      'invoice_number': event.target.invoiceNumber.value,
-      'due_date': event.target.dueDate.value,
-      'notes': event.target.notes.value,
-      'clientId': clientId
-    }
+    .then( response => {
+      const invoiceId = response.data.data.id
 
-    const newLineItems = []
-
-    const descriptions = Array.from(document.querySelectorAll('.desc'))
+      const descriptions = Array.from(document.querySelectorAll('.desc'))
       .map(description => description.value)
-
-    const quantities = Array.from(document.querySelectorAll('.qty'))
+      
+      const quantities = Array.from(document.querySelectorAll('.qty'))
       .map(quantity => quantity.value)
-
-    const rates = Array.from(document.querySelectorAll('.rate'))
+      
+      const rates = Array.from(document.querySelectorAll('.rate'))
       .map(rate => rate.value)
+      
+      const newLineItems = []
 
-
-    createInvoice(newInvoice)
-      .then((response) => {
-
-        const invoiceId = response.data.data.id
-
-        for (i = 0; i < descriptions.length; i++) {
+      for (i = 0; i < descriptions.length; i++) {
+        
           const lineItem = {
             description: descriptions[i],
             quantity: quantities[i],
