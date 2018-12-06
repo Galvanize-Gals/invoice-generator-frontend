21b42f538d807907d53d7b00c81a1b49a4ce6d08
Jamie Kinkead <jkinkead@gmail.com>
2018-12-05 16:28:45 -0800

    manage in progress

--- scripts/manage.js
+++ scripts/manage.js
@@ -1,14 +1,42 @@
 const { getAllClientInvoices } = require('./requests')
-const { invoiceLine } = require('./templates')
+const { vendorInvoiceLine } = require('./templates')
+const { togglePaid } = require('./requests')
 
-function init(){
+function init() {
     const invoiceList = document.querySelector('.invoice-list')
     getAllClientInvoices()
         .then((res) => {
-            const invoiceItem = res.data.data.map(a => invoiceLine(a) )
+            const invoiceItem = res.data.data.map(a => vendorInvoiceLine(a))
             invoiceList.innerHTML = ''
             invoiceList.innerHTML = invoiceItem.join('\n')
+
         })
+    const addOutstandingEventListeners = () => {
+        const outstandingArray = document.querySelectorAll('.outstanding')
+        for (ele of outstandingArray) {
+            ele.addEventListener('click', (e) => {
+                togglePaid(e.target.getAttribute(`data-id`))
+                //reRender page ()
+                addOutstandingEventListeners()
+
+            })
+        }
+    }
+
+    const addPaidEventListeners = () => {
+        const paidArray = document.querySelectorAll('.paid')
+        for (ele of paidArray) {
+            ele.addEventListener('click', (e) => {
+                togglePaid()
+                //reRender page ()
+                addPaidEventListeners()
+
+            })
+        }
+    }
+
+    addOutstandingEventListeners()
+    addPaidEventListeners()
 }
 
-module.exports = {init}
+module.exports = { init }
